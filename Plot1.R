d978ed69618f02a3d0568d80ebd9c0ebf4327171
halberdzc <halberdzc@gmail.com>
2015-06-07 16:45:19 -0700

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,3 @@
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data.final$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
